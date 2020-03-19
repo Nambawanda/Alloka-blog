5e70e9c36ce3b0147eb414051768bc4735413a90
flaunder007 <ilja@sawtech.ru>
2020-03-19 17:44:13 +0300

    bug fix

--- wp-content/themes/alloka/header_v2.php
+++ wp-content/themes/alloka/header_v2.php
@@ -9,6 +9,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="format-detection" content="telephone=no" />
 	<meta name="google" content="notranslate" />
+	<link href="https://alloka.ru/assets/favicon-6db90c830b3a30fa0c8a1742113e707cab7a8ff82c7c4e942e15a7cd9523970e.ico" rel="shortcut icon">
+	<link href="https://alloka.ru/assets/apple-touch-icon-bad818d5da80addfb440da42bbbecebc0ea599ab4b1340c3aeb72ad23e33d648.png" rel="apple-touch-icon">
+	<link href="https://alloka.ru/assets/apple-touch-icon-72x72-3850f7020fd01568410a970bb8137647e4c9fc1d14670fa4f73428567cc043df.png" rel="apple-touch-icon" sizes="72x72">
+	<link href="https://alloka.ru/assets/apple-touch-icon-114x114-03fb12251d7cd23f6fbc6bfd46b9e028e5b27abc3b2fd8a0f958b8692e203928.png" rel="apple-touch-icon" sizes="114x114">
 	<!-- WP-head -->
 	<?php wp_head();?>
 	<!-- WP-head -->
