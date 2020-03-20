22ff558ae1e3cffb487471da020f1900e928505e
flaunder007 <ilja@sawtech.ru>
2020-03-20 12:10:30 +0300

    header fix

--- wp-content/themes/alloka/header_v2.php
+++ wp-content/themes/alloka/header_v2.php
@@ -2,6 +2,9 @@
 <html lang="ru">
 <head>
 	<title><?=wp_title()?></title>
+	<!-- WP-head -->
+	<?php wp_head();?>
+	<!-- WP-head -->
 	<!-- Meta -->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
@@ -13,9 +16,6 @@
 	<link href="https://alloka.ru/assets/apple-touch-icon-bad818d5da80addfb440da42bbbecebc0ea599ab4b1340c3aeb72ad23e33d648.png" rel="apple-touch-icon">
 	<link href="https://alloka.ru/assets/apple-touch-icon-72x72-3850f7020fd01568410a970bb8137647e4c9fc1d14670fa4f73428567cc043df.png" rel="apple-touch-icon" sizes="72x72">
 	<link href="https://alloka.ru/assets/apple-touch-icon-114x114-03fb12251d7cd23f6fbc6bfd46b9e028e5b27abc3b2fd8a0f958b8692e203928.png" rel="apple-touch-icon" sizes="114x114">
-	<!-- WP-head -->
-	<?php wp_head();?>
-	<!-- WP-head -->
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/css/style.css">
 </head>
